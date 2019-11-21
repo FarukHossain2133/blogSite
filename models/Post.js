a781ad4801771b7d374aa2193b0f0df70031334a
faruk <fa905432@gmail.com>
2019-11-21 09:39:05 +0600

    fixes dbCollection

--- models/Post.js
+++ models/Post.js
@@ -1,5 +1,6 @@
 const mongoose =  require('mongoose');
- mongoose.connect('mongodb://localhost:27017/postCollection', {useNewUrlParser: true, useCreateIndex: true} || 'mongodb+srv://Faruk:Faruk01936@cluster0-fqsei.mongodb.net/BlogPost?retryWrites=true&w=majority', {useNewUrlParser: true})
+//  mongoose.connect('mongodb://localhost:27017/postCollection', {useNewUrlParser: true, useCreateIndex: true} || 'mongodb+srv://Faruk:Faruk01936@cluster0-fqsei.mongodb.net/BlogPost?retryWrites=true&w=majority', {useNewUrlParser: true})
+ mongoose.connect('mongodb+srv://Faruk:Faruk01936@cluster0-fqsei.mongodb.net/BlogPost?retryWrites=true&w=majority', {useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true})
 const conn =  mongoose.connection;
 
 const blogPostSchema =  new mongoose.Schema({
